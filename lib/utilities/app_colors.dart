25c53bfe1b90e1663beb481a223eec8688963ce3
Abdul Azis <abdul.azizz1108@gmail.com>
2022-11-10 21:58:45 +0700

    add image and app colors

---
+++ lib/utilities/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const purple = Color(0xff6C5ECF);
+  static const lightBlue = Color(0xff38ABBE);
+  static const redColor = Color(0xffED6363);
+  static const blue = Color(0xff2C96F1);
+  static const black1 = Color(0xff1F1D2B);
+  static const black2 = Color(0xff2B2937);
+  static const black3 = Color(0xff242231);
+  static const white = Color(0xffFFFFFF);
+  static const grey = Color(0xff999999);
+}
