756275caa0fa02dfcfbdee2b40290d5d518a5fd7
Abdul Azis <abdul.azizz1108@gmail.com>
2022-11-16 10:45:43 +0700

    create chat message empty content

--- lib/presentation/pages/chat_page.dart
+++ lib/presentation/pages/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/presentation/widgets/filled_button.dart';
 import 'package:shamo_app/utilities/app_colors.dart';
 import 'package:shamo_app/utilities/font_weight.dart';
 
@@ -7,9 +8,7 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Chat Page"),
-    );
+    return _emptyContent(context: context);
   }
 
   static AppBar appBar({required BuildContext context}) {
@@ -24,6 +23,50 @@ class ChatPage extends StatelessWidget {
             ),
       ),
       centerTitle: true,
+    );
+  }
+
+  Widget _emptyContent({required BuildContext context}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          "assets/ic-headset.png",
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          "Opss no message yet?",
+          style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                color: AppColors.white,
+                fontWeight: medium,
+              ),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        Text(
+          "You have never done a transaction",
+          style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                color: AppColors.grey,
+              ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        FilledButton(
+          width: 152,
+          onPressed: () {},
+          child: Text(
+            "Explore Store",
+            style: Theme.of(context).textTheme.button?.copyWith(
+                  color: AppColors.white,
+                ),
+          ),
+        ),
+      ],
     );
   }
 }
