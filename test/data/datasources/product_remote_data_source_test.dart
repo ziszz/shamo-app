e6213f4101bfed652f2bba3c239618f268484d26
Abdul Azis <abdul.azizz1108@gmail.com>
2022-11-19 14:15:53 +0700

    create failure get product testing

--- test/data/datasources/product_remote_data_source_test.dart
+++ test/data/datasources/product_remote_data_source_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
 import 'package:shamo_app/data/datasources/product_remote_data_source.dart';
+import 'package:shamo_app/utilities/exceptions.dart';
 
 import '../../dummy_data/dummy_object.dart';
 import '../../helpers/test_helper.mocks.dart';
@@ -26,12 +27,19 @@ void main() {
     test("should return list of Product Model when the response code is 200",
         () async {
       when(mockIOClient.get(Uri.parse("${dotenv.env['apiUrl']}/api/products")))
-          .thenAnswer(
-        (_) async => http.Response(readJson("dummy_data/product.json"), 200),
-      );
-
+          .thenAnswer((_) async =>
+              http.Response(readJson("dummy_data/product.json"), 200));
       final result = await dataSource.getProduct();
       expect(result, equals(testProductModelList));
+    });
+
+    test(
+        "should throw a server exception when the response code is 404 or other",
+        () async {
+      when(mockIOClient.get(Uri.parse("${dotenv.env['apiUrl']}/api/products")))
+          .thenAnswer((_) async => http.Response("Not Found", 404));
+      final result = dataSource.getProduct();
+      expect(result, throwsA(isA<ServerException>()));
     });
   });
 }
