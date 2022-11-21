52f17ae8cfc5a6717768fbf5db20283971445721
Abdul Azis <abdul.azizz1108@gmail.com>
2022-11-21 14:17:19 +0700

    create get product in product repository

--- test/data/repositories/product_repository_impl_test.dart
+++ test/data/repositories/product_repository_impl_test.dart
@@ -1,13 +1,15 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:shamo_app/data/repositories/product_repository_impl.dart';
-import 'package:shamo_app/domain/repositories/product_repository.dart';
+import 'package:shamo_app/utilities/exceptions.dart';
+import 'package:shamo_app/utilities/failure.dart';
 
 import '../../dummy_data/dummy_object.dart';
 import '../../helpers/test_helper.mocks.dart';
 
 void main() {
-  late ProductRepository repository;
+  late ProductRepositoryImpl repository;
   late MockProductRemoteDataSource mockRemoteDataSource;
 
   setUp(() {
@@ -29,6 +31,17 @@ void main() {
       final resultList = result.getOrElse(() => []);
       // assert
       expect(resultList, [testProduct]);
+    });
+
+    test(
+        "should return server failure when the call to remote data source is failed",
+        () async {
+      // arrange
+      when(mockRemoteDataSource.getProducts()).thenThrow(ServerException());
+      // act
+      final result = await repository.getProducts();
+      // assert
+      expect(result, const Left(ServerFailure("")));
     });
   });
 }
