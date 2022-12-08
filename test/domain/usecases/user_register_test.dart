c2872c7015a66a65d735d2aa5fa007ec8c703f96
Abdul Azis <abdul.azizz1108@gmail.com>
2022-12-08 18:21:33 +0700

    fix use register use case testing error

--- test/domain/usecases/user_register_test.dart
+++ test/domain/usecases/user_register_test.dart
@@ -29,12 +29,12 @@ void main() {
       // arrange
       when(mockAuthRepository.register(
               testName, testEmail, testUsername, testPass))
-          .thenAnswer((_) async => const Right(testUser));
+          .thenAnswer((_) async => const Right(testUserFromUserReponse));
       // act
       final result =
           await usecase.execute(testName, testEmail, testUsername, testPass);
       // assert
-      expect(result, const Right(testUser));
+      expect(result, const Right(testUserFromUserReponse));
     });
 
     test("should get failure message when get from repository is failed",
