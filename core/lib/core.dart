ff8b13e53e0cbd581105c156dde090a89e2bd49b
Abdul Azis <abdul.azizz1108@gmail.com>
2022-12-18 20:03:35 +0700

    fix core module error and move router.dart file to app module

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -4,5 +4,4 @@ export 'utilities/app_colors.dart';
 export 'utilities/constants.dart';
 export 'utilities/exceptions.dart';
 export 'utilities/failure.dart';
-export 'utilities/router.dart';
 export 'utilities/ssl.dart';
